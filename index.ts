dd0c1e74900ea8d05f23aeb2d8ffc8b900cfe143
vikas <vikaskumar783588@gmail.com>
2025-11-24 22:24:28 +0530

    changes peerdependency

--- index.ts
+++ index.ts
@@ -1,6 +1,2 @@
 export { default as SpiClient } from "./spi_client";
 export { default as SpiServer } from "./spi_server"
-
-export * as anchor  from "@coral-xyz/anchor";
-export * as solanapay from "@solana/pay";
-export * as solanakit from "@solana/web3.js";
