d788236ee04c42e37921dc05f0d8c86a6667841b
vikas <vikaskumar783588@gmail.com>
2025-11-24 22:08:05 +0530

    few changes

--- index.ts
+++ index.ts
@@ -1,2 +1,6 @@
 export { default as SpiClient } from "./spi_client";
-export { default as SpiServer } from "./spi_server";
+export { default as SpiServer } from "./spi_server"
+
+export * as anchor  from "@coral-xyz/anchor";
+export * as solanapay from "@solana/pay";
+export * as solanakit from "@solana/web3.js";
